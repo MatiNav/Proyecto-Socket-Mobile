d909d6a3527a7c0007af6c2f6f9b09e401358a6d
Matias Navarro <mnavarro@MacBook-Pro.local>
2018-11-10 14:55:34 -0300

    agregado del public

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-
+const path = require('path');
 const socketSrvc = require('../server/socket/socket');
 
 const app = express();
@@ -8,6 +8,12 @@ const app = express();
 const server = http.createServer(app);
 
 socketSrvc.initSocket(server);
+
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(publicPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
